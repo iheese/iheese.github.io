f2a8b06f2743f6aff3b62a9e271a8616afdafaeb
ddungi <imbc0224@naver.com>
2021-10-13 11:39:41 +0900

    add search tool but we have error we gonna  change

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -4,6 +4,7 @@
 # v1.4
 layout: null
 ---
+
 {%- assign index = "" | split: "" -%}
 {%- assign excluded_files = site.tipue_search.exclude.files -%}
 {%- assign excluded_tags = site.tipue_search.exclude.tags | uniq -%}
